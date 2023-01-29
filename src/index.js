f223e033e7bdd0c350f03eaaa7f12ce973037dbb
Simon <sema92m@wp.pl>
2023-01-29 18:38:51 +0100

    Day 4 complete

--- src/index.js
+++ src/index.js
@@ -5,186 +5,249 @@ import elephant from "./images/elephant.png";
 import gorilla from "./images/gorilla.png";
 import home from "./images/home.png";
 import ice from "./images/ice_cream.png";
+import checked from './images/checked.svg'
 
-const welcome = "Welcome to 30 Days Of React";
-const title = "Getting Started React";
-const subtitle = "JavaScript Library";
-const author = {
-    firstName: "Asabeneh",
-    lastName: "Yetayeh",
-};
-const date = "Oct 3, 2020";
-const Header = () => (
-    <header>
-        <div className="header-wrapper">
-            <h1>{welcome}</h1>
-            <h2>{title}</h2>
-            <h3>{subtitle}</h3>
-            <p>
-                Student: {author.firstName} {author.lastName}
-            </p>
-            <strong>{date}</strong>
-        </div>
-    </header>
-);
-const numOne = 3;
-const numTwo = 2;
-
-const result = (
-    <p>
-        {numOne} + {numTwo} = {numOne + numTwo}
-    </p>
-);
-const yearBorn = 1820;
-const currentYear = 2020;
-const age = currentYear - yearBorn;
-const personAge = (
-    <p>
-        {" "}
-        {author.firstName} {author.lastName} is {age} years old
-    </p>
-);
-const UserCard = () => (
-    <div className="user-card">
-        <img src={myImg} alt="my" />
-        <h2>
-            Author:{author.firstName} {author.lastName}
-        </h2>
-    </div>
-);
-
-const techs = ["HTML", "CSS", "JS"];
-const techsFormatted = techs.map((tech) => <li key={tech}>{tech}</li>);
-
-const Main = () => (
-    <main>
-        <div className="main-wrapper">
-            <div>
-                <p>
-                    Prerequisite to get started{" "}
-                    <strong>
-                        <em>react.js</em>
-                    </strong>
-                    :
-                </p>
-                <ul>{techsFormatted}</ul>
-                {result}
-                {personAge}
-            </div>
-            <UserCard />
-        </div>
-    </main>
-);
-
-const copyRight = "2020";
-const Footer = () => (
-    <footer>
-        <div className="footer-wrapper">
-            <p>Copyright & copy {copyRight}</p>
-        </div>
-    </footer>
-);
-
-const hexaColor = () => {
-    let str = "0123456789abcdef";
-    let color = "";
-    for (let i = 0; i < 6; i++) {
-        let index = Math.floor(Math.random() * str.length);
-        color += str[index];
-    }
-    return "#" + color;
-};
-
-// const HexaColor = () => <div>{hexaColor()}</div>;
-
-// const buttonStyles = {
-//     padding: "10px 20px",
-//     background: "rgb(0, 255, 0",
-//     border: "none",
-//     borderRadius: 5,
-//     cursor: "pointer",
+// const welcome = "Welcome to 30 Days Of React";
+// const title = "Getting Started React";
+// const subtitle = "JavaScript Library";
+// const author = {
+//     firstName: "Asabeneh",
+//     lastName: "Yetayeh",
 // };
+// const date = "Oct 3, 2020";
+// const Header = () => (
+//     <header>
+//         <div className="header-wrapper">
+//             <h1>{welcome}</h1>
+//             <h2>{title}</h2>
+//             <h3>{subtitle}</h3>
+//             <p>
+//                 Student: {author.firstName} {author.lastName}
+//             </p>
+//             <strong>{date}</strong>
+//         </div>
+//     </header>
+// );
+// const numOne = 3;
+// const numTwo = 2;
 
-// const Button = () => <button style={buttonStyles}> action </button>;
-// const Input1 = () => <input></input>;
-
-// const Alert = () => (
-//     <div>
-//         <p>AllClear</p>
+// const result = (
+//     <p>
+//         {numOne} + {numTwo} = {numOne + numTwo}
+//     </p>
+// );
+// const yearBorn = 1820;
+// const currentYear = 2020;
+// const age = currentYear - yearBorn;
+// const personAge = (
+//     <p>
+//         {" "}
+//         {author.firstName} {author.lastName} is {age} years old
+//     </p>
+// );
+// const UserCard = () => (
+//     <div className="user-card">
+//         <img src={myImg} alt="my" />
+//         <h2>
+//             Author:{author.firstName} {author.lastName}
+//         </h2>
 //     </div>
 // );
-// const cardsStyles = {
+
+// const techs = ["HTML", "CSS", "JS"];
+// const techsFormatted = techs.map((tech) => <li key={tech}>{tech}</li>);
+
+// const Main = () => (
+//     <main>
+//         <div className="main-wrapper">
+//             <div>
+//                 <p>
+//                     Prerequisite to get started{" "}
+//                     <strong>
+//                         <em>react.js</em>
+//                     </strong>
+//                     :
+//                 </p>
+//                 <ul>{techsFormatted}</ul>
+//                 {result}
+//                 {personAge}
+//             </div>
+//             <UserCard />
+//         </div>
+//     </main>
+// );
+
+// const copyRight = "2020";
+// const Footer = () => (
+//     <footer>
+//         <div className="footer-wrapper">
+//             <p>Copyright & copy {copyRight}</p>
+//         </div>
+//     </footer>
+// );
+
+// const hexaColor = () => {
+//     let str = "0123456789abcdef";
+//     let color = "";
+//     for (let i = 0; i < 6; i++) {
+//         let index = Math.floor(Math.random() * str.length);
+//         color += str[index];
+//     }
+//     return "#" + color;
+// };
+
+// // const HexaColor = () => <div>{hexaColor()}</div>;
+
+// // const buttonStyles = {
+// //     padding: "10px 20px",
+// //     background: "rgb(0, 255, 0",
+// //     border: "none",
+// //     borderRadius: 5px,
+// //     cursor: "pointer",
+// // };
+
+// // const Button = () => <button style={buttonStyles}> action </button>;
+// // const Input1 = () => <input></input>;
+
+// // const Alert = () => (
+// //     <div>
+// //         <p>AllClear</p>
+// //     </div>
+// // );
+// // const cardsStyles = {
+// //     display: "flex",
+// //     flexDirection: "column",
+// //     alignItems: "center",
+// // };
+// // const Card = () => (
+// //     <div style={cardsStyles}>
+// //         <h3>Technologies</h3>
+// //         <div>
+// //             <img src={elephant} alt="el" />
+// //             <img src={gorilla} alt="gorilla" />
+// //             <img src={home} alt="home" />
+// //             <img src={ice} alt="ice" />
+// //         </div>
+// //     </div>
+// // );
+
+// //Exercise 2--------------------------------------
+// const cardStyle = {
 //     display: "flex",
 //     flexDirection: "column",
+//     justifyContent: "space-around",
 //     alignItems: "center",
+//     width: "85%",
+//     height: "200px",
+//     margin: "20px auto",
+//     background: "lightblue",
+//     borderRadius: "10px",
 // };
-// const Card = () => (
-//     <div style={cardsStyles}>
-//         <h3>Technologies</h3>
+
+// const inputStyles = {
+//     width: "140px",
+//     padding: "7px 5px ",
+//     margin: "10px",
+//     fontSize: "10px",
+//     borderRadius: "5px",
+//     border: "none",
+// };
+// const buttonStyles = {
+//     width: "150px",
+//     padding: "5px 10px",
+//     background: "orangered",
+//     color: "white",
+//     borderRadius: "5px",
+//     border: "none",
+// };
+// const SubscribeCard = () => (
+//     <div style={cardStyle}>
+//         <h3>SUBSCRIBE</h3>
+//         <h4>Sing up with your address to receive news and updates.</h4>
 //         <div>
-//             <img src={elephant} alt="el" />
-//             <img src={gorilla} alt="gorilla" />
-//             <img src={home} alt="home" />
-//             <img src={ice} alt="ice" />
+//             <input style={inputStyles} placeholder="First Name"></input>
+//             <input style={inputStyles} placeholder="Last Name"></input>
+//             <input style={inputStyles} placeholder="Email"></input>
 //         </div>
+//         <button style={buttonStyles}>Subscribe</button>
 //     </div>
 // );
+//Exercise 2--------------------------------------------
 
+//Exercise 3.1-------------------------------------------
+// const hexaColor = () => {
+//     let str = "0123456789abcdef";
+//     let color = "";
+//     for (let i = 0; i < 6; i++) {
+//         let index = Math.floor(Math.random() * str.length);
+//         color += str[index];
+//     }
+//     return "#" + color;
+// };
 
+// const paragraphStyles = {
+//     height: '100%',
+//     display: 'flex',
+//     alignItems: "center",
+//     justifyContent: 'center',
+//     fontSize: "25px",
+//     color: "white",
+//     fontWeight: "900",
+//     letterSpacing: "2px",
+// };
 
-const cardStyle = {
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'space-around',
-  alignItems: 'center',
-  width: '85%',
-  height: '200px',
-  margin: '20px auto',
-  background: 'lightblue',
-  borderRadius: '10px',
-}
-
-const inputStyles = {
-  width: '140px',
-  padding: '7px 5px ',
-  margin: '10px',
-  fontSize: '10px',
-  borderRadius: '5px',
-  border: 'none',
-}
-const buttonStyles ={
-  width: '150px',
-  padding: '5px 10px',
-  background: 'orangered',
-  color: 'white',
-  borderRadius: '5px',
-  border: 'none',
-}
-const SubscribeCard = () => (
-  <div style={cardStyle}>
-    <h3>SUBSCRIBE</h3>
-    <h4>Sing up with your address to receive news and updates.</h4>
+// const Block = () => (
+//     <div style={{width: "80%",height: "70px",margin: "5px auto",borderRadius: "5px", background: hexaColor() }}>
+//         <p style={paragraphStyles}>{hexaColor()}</p>
+//     </div>
+// );
+//Exercise 3.1-------------------------------------------
+//Exercise 3.2-------------------------------------------
+const myInfo = {
+    firstName: "Semion",
+    lastName: "Bryczkowski",
+    exp: "Senior Developer",
+    country: "Finland",
+};
+const MyData = () => (
     <div>
-      <input style={inputStyles} placeholder="First Name"></input>
-      <input style={inputStyles} placeholder="Last Name"></input>
-      <input style={inputStyles} placeholder="Email"></input>
+        <img className="my-foto" src={myImg} alt="myImg" />
+        <div className="my-name my-info">
+            <p>{myInfo.firstName} {myInfo.lastName}  <img className="checked-svg" src={checked} alt="checked" /></p>
+            
+        </div> 
+        <p>
+            {myInfo.exp}, {myInfo.country}
+        </p>
     </div>
-    <button style={buttonStyles}>Subscribe</button>
-  </div>
-)
+);
 
+const skills = ["JS", "HTML", "CSS", "React", "Node", "Python", "Flask", "Git"];
+const skillsLI = skills.map((skill) => (
+    <li className="skills-li" key={skill}>
+        {skill}
+    </li>
+));
 
+const MySkills = () => (
+    <div >
+        <h3 className="skills-header">SKILLS</h3>
+        <ul className="skills-wrapper">{skillsLI}</ul>
+    </div>
+);
 
-
+const Footer = () => (
+    <div className="footer3">
+        <p>🕑 Joined on Aug 30, 2020</p>
+    </div>
+);
+//Exercise 3.2-------------------------------------------
 
 const App = () => (
-    <div className="app">
-        {/* <Header />
-      <Main />
-      <Footer /> */}
-        <SubscribeCard />
-        {/* <Button /> */}
-        {/* <Input1 /> */}
+    <div className="container3">
+        <MyData />
+        <MySkills />
+        <Footer />
     </div>
 );
 
