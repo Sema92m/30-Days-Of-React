1b4c4aa41e05da77d4956012d96a48c03651eb42
Simon <sema92m@wp.pl>
2023-02-01 20:57:07 +0100

    Add ex 3.1 day 5

--- src/index.js
+++ src/index.js
@@ -6,72 +6,121 @@ import gorilla from "./images/gorilla.png";
 import home from "./images/home.png";
 import ice from "./images/ice_cream.png";
 
-const cardStyle = {
+// exercise 2 CARDS
+// const cardStyle = {
+//     display: "flex",
+//     flexDirection: "column",
+//     justifyContent: "space-around",
+//     alignItems: "center",
+//     width: "70%",
+//     height: "200px",
+//     margin: "20px auto",
+//     background: "lightblue",
+//     borderRadius: "10px",
+// };
+
+// const inputStyles = {
+//     width: "140px",
+//     padding: "7px 5px ",
+//     margin: "10px",
+//     fontSize: "10px",
+//     borderRadius: "5px",
+//     border: "none",
+// };
+// const buttonStyles = {
+//     width: "150px",
+//     padding: "5px 10px",
+//     background: "orangered",
+//     color: "white",
+//     borderRadius: "5px",
+//     border: "none",
+//     cursor: 'pointer',
+// };
+
+// const Button = ({text, style}) => (
+//   <button style={style}>{text}</button>
+// )
+
+// const SubscribeCard = ({ data: { title, subscribe,placeholderFN,placeholderLN,placeholderEmail } }) => {
+//     return (
+//         <div style={cardStyle}>
+//             <h3>{subscribe.toUpperCase()}</h3>
+//             <h4>{title}</h4>
+//             <div>
+//                 <input style={inputStyles} placeholder={placeholderFN}></input>
+//                 <input style={inputStyles} placeholder={placeholderLN}></input>
+//                 <input style={inputStyles} placeholder={placeholderEmail}></input>
+//             </div>
+//             <Button text = 'Subscribe' style={buttonStyles}></Button>
+//             {/* <button style={buttonStyles}>{subscribe}</button> */}
+//         </div>
+//     );
+// };
+
+// const App = () => {
+//   const data = {
+//     subscribe: "Subscribe",
+//     title: "Sing up with your address to receive news and updates.",
+//     placeholderFN: "First Name",
+//     placeholderLN: "Last Name",
+//     placeholderEmail: "Email",
+
+// };
+//     return (
+//         <div className="app">
+//             <SubscribeCard data={data} />
+//         </div>
+//     );
+// };
+// exercise 2 CARDS
+
+// exercise 3.1 COLORS
+
+const paragraphStyles = {
+    height: "100%",
     display: "flex",
-    flexDirection: "column",
-    justifyContent: "space-around",
     alignItems: "center",
-    width: "70%",
-    height: "200px",
-    margin: "20px auto",
-    background: "lightblue",
-    borderRadius: "10px",
-};
-
-const inputStyles = {
-    width: "140px",
-    padding: "7px 5px ",
-    margin: "10px",
-    fontSize: "10px",
-    borderRadius: "5px",
-    border: "none",
-};
-const buttonStyles = {
-    width: "150px",
-    padding: "5px 10px",
-    background: "orangered",
+    justifyContent: "center",
+    fontSize: "25px",
     color: "white",
+    fontWeight: "900",
+    letterSpacing: "2px",
+};
+
+const blockColorStyles = {
+    width: "80%",
+    height: "70px",
+    margin: "5px auto",
     borderRadius: "5px",
-    border: "none",
-    cursor: 'pointer',
 };
 
-const Button = ({text, style}) => (
-  <button style={style}>{text}</button>
-)
-
-const SubscribeCard = ({ data: { title, subscribe,placeholderFN,placeholderLN,placeholderEmail } }) => {
-    return (
-        <div style={cardStyle}>
-            <h3>{subscribe.toUpperCase()}</h3>
-            <h4>{title}</h4>
-            <div>
-                <input style={inputStyles} placeholder={placeholderFN}></input>
-                <input style={inputStyles} placeholder={placeholderLN}></input>
-                <input style={inputStyles} placeholder={placeholderEmail}></input>
-            </div>
-            <Button text = 'Subscribe' style={buttonStyles}></Button>
-            {/* <button style={buttonStyles}>{subscribe}</button> */}
-        </div>
-    );
-};
-
+const Block = ({ hexaColor }) => (
+    <div style={{ ...blockColorStyles, background: hexaColor }}>
+        <p style={paragraphStyles}>{hexaColor}</p>
+    </div>
+);
+// exercise 3.1 COLORS
 const App = () => {
-  const data = {
-    subscribe: "Subscribe",
-    title: "Sing up with your address to receive news and updates.",
-    placeholderFN: "First Name",
-    placeholderLN: "Last Name",
-    placeholderEmail: "Email",
-    
-};
+    const hexaColor = () => {
+        let str = "0123456789abcdef";
+        let color = "";
+        for (let i = 0; i < 6; i++) {
+            let index = Math.floor(Math.random() * str.length);
+            color += str[index];
+        }
+        return "#" + color;
+    };
     return (
         <div className="app">
-            <SubscribeCard data={data} />
+            <Block hexaColor={hexaColor()} />
+            <Block hexaColor={hexaColor()} />
+            <Block hexaColor={hexaColor()} />
+            <Block hexaColor={hexaColor()} />
+            <Block hexaColor={hexaColor()} />
+            <Block hexaColor={hexaColor()} />
         </div>
     );
 };
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
--- src/tecnicalFile.js
+++ src/tecnicalFile.js
@@ -1,63 +1,140 @@
-import myImg from "./images/img1.png";
-import elephant from "./images/elephant.png";
-import gorilla from "./images/gorilla.png";
-import home from "./images/home.png";
-import ice from "./images/ice_cream.png";
+// import myImg from "./images/img1.png";
+// import elephant from "./images/elephant.png";
+// import gorilla from "./images/gorilla.png";
+// import home from "./images/home.png";
+// import ice from "./images/ice_cream.png";
 
 
 
-const cardStyle = {
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'space-around',
-  alignItems: 'center',
-  width: '85%',
-  height: '200px',
-  margin: '20px auto',
-  background: 'lightblue',
-  borderRadius: '10px',
-}
+// const cardStyle = {
+//   display: 'flex',
+//   flexDirection: 'column',
+//   justifyContent: 'space-around',
+//   alignItems: 'center',
+//   width: '85%',
+//   height: '200px',
+//   margin: '20px auto',
+//   background: 'lightblue',
+//   borderRadius: '10px',
+// }
 
-const inputStyles = {
-  width: '140px',
-  padding: '7px 5px ',
-  margin: '10px',
-  fontSize: '10px',
-  borderRadius: '5px',
-  border: 'none',
-}
-const buttonStyles ={
-  width: '150px',
-  padding: '5px 10px',
-  background: 'orangered',
-  color: 'white',
-  borderRadius: '5px',
-  border: 'none',
-}
-const SubscribeCard = () => (
-  <div style={cardStyle}>
-    <h3>SUBSCRIBE</h3>
-    <h4>Sing up with your address to receive news and updates.</h4>
-    <div>
-      <input style={inputStyles} placeholder="First Name"></input>
-      <input style={inputStyles} placeholder="Last Name"></input>
-      <input style={inputStyles} placeholder="Email"></input>
-    </div>
-    <button style={buttonStyles}>Subscribe</button>
-  </div>
-)
-
-
+// const inputStyles = {
+//   width: '140px',
+//   padding: '7px 5px ',
+//   margin: '10px',
+//   fontSize: '10px',
+//   borderRadius: '5px',
+//   border: 'none',
+// }
+// const buttonStyles ={
+//   width: '150px',
+//   padding: '5px 10px',
+//   background: 'orangered',
+//   color: 'white',
+//   borderRadius: '5px',
+//   border: 'none',
+// }
+// const SubscribeCard = () => (
+//   <div style={cardStyle}>
+//     <h3>SUBSCRIBE</h3>
+//     <h4>Sing up with your address to receive news and updates.</h4>
+//     <div>
+//       <input style={inputStyles} placeholder="First Name"></input>
+//       <input style={inputStyles} placeholder="Last Name"></input>
+//       <input style={inputStyles} placeholder="Email"></input>
+//     </div>
+//     <button style={buttonStyles}>Subscribe</button>
+//   </div>
+// )
 
 
 
-const App = () => (
-    <div className="app">
-        {/* <Header />
-      <Main />
-      <Footer /> */}
-        <SubscribeCard />
-        {/* <Button /> */}
-        {/* <Input1 /> */}
-    </div>
-);
+
+
+// const App = () => (
+//     <div className="app">
+//         {/* <Header />
+//       <Main />
+//       <Footer /> */}
+//         <SubscribeCard />
+//         {/* <Button /> */}
+//         {/* <Input1 /> */}
+//     </div>
+// );
+
+// //Exercise 3.1-------------------------------------------
+// // const hexaColor = () => {
+// //     let str = "0123456789abcdef";
+// //     let color = "";
+// //     for (let i = 0; i < 6; i++) {
+// //         let index = Math.floor(Math.random() * str.length);
+// //         color += str[index];
+// //     }
+// //     return "#" + color;
+// // };
+
+// // const paragraphStyles = {
+// //     height: '100%',
+// //     display: 'flex',
+// //     alignItems: "center",
+// //     justifyContent: 'center',
+// //     fontSize: "25px",
+// //     color: "white",
+// //     fontWeight: "900",
+// //     letterSpacing: "2px",
+// // };
+
+// // const Block = () => (
+// //     <div style={{width: "80%",height: "70px",margin: "5px auto",borderRadius: "5px", background: hexaColor() }}>
+// //         <p style={paragraphStyles}>{hexaColor()}</p>
+// //     </div>
+// // );
+// //Exercise 3.1-------------------------------------------
+// //Exercise 3.2-------------------------------------------
+// const myInfo = {
+//   firstName: "Semion",
+//   lastName: "Bryczkowski",
+//   exp: "Senior Developer",
+//   country: "Finland",
+// };
+// const MyData = () => (
+//   <div>
+//       <img className="my-foto" src={myImg} alt="myImg" />
+//       <div className="my-name my-info">
+//           <p>{myInfo.firstName} {myInfo.lastName}  <img className="checked-svg" src={checked} alt="checked" /></p>
+          
+//       </div> 
+//       <p>
+//           {myInfo.exp}, {myInfo.country}
+//       </p>
+//   </div>
+// );
+
+// const skills = ["JS", "HTML", "CSS", "React", "Node", "Python", "Flask", "Git"];
+// const skillsLI = skills.map((skill) => (
+//   <li className="skills-li" key={skill}>
+//       {skill}
+//   </li>
+// ));
+
+// const MySkills = () => (
+//   <div >
+//       <h3 className="skills-header">SKILLS</h3>
+//       <ul className="skills-wrapper">{skillsLI}</ul>
+//   </div>
+// );
+
+// const Footer = () => (
+//   <div className="footer3">
+//       <p>🕑 Joined on Aug 30, 2020</p>
+//   </div>
+// );
+// //Exercise 3.2-------------------------------------------
+
+// const App = () => (
+//   <div className="container3">
+//       <MyData />
+//       <MySkills />
+//       <Footer />
+//   </div>
+// );
