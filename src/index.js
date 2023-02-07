1d53f7724910090ab945a98c157d49625527c55a
Sema92m <sema92m@wp.pl>
2023-02-07 22:25:16 +0100

    Ex 2.2 day 6 complete

--- src/index.js
+++ src/index.js
@@ -2,44 +2,35 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-function isPrime(num) {
-    if (num <= 1) return false;
-    for (let i = 2; i < num; i++) {
-        if (num % i === 0) {
-            return false;
-        }
+const hexaColor = () => {
+    const str = "0123456789abcdef";
+    let hexa = "#";
+    let index;
+    for (let i = 0; i < 6; i++) {
+        index = Math.floor(Math.random() * str.length);
+        hexa += str[index];
     }
-    return true;
-}
-
-const Numbers = ({ numbers }) => {
-    const numList = numbers.map((num, index) => {
-        let className;
-        if (isPrime(num)) {
-            className = "numbers-li-red";
-        } else if (index % 2 !== 0) {
-            className = "numbers-li-yellow";
-        } else {
-            className = "numbers-li";
-        }
-        return (
-            <li key={num} className={className}>
-                {num}
-            </li>
-        );
-    });
-    return <ul className="numbers-grid">{numList}</ul>
+    return hexa;
 };
 
 
+const Colors = () => {
+    let arr= [];
+    for (let i = 0; i < 32; i++) {
+        arr.push(hexaColor());
+    }
+    const color1 = arr.map((elem) => (
+        <li key={elem} className="li-colors" style={{ backgroundColor: elem }}>
+            {elem}
+        </li>
+    ));
+    return <ul className="numbers-grid">{color1}</ul>;
+};
+
 const App = () => {
-    const numbers = [
-        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
-        20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
-    ];
     const data = {
         title: "30 Days Of React",
-        subtitle: "Number Generator",
+        subtitle: "Hexadencimal Colors",
     };
 
     return (
@@ -48,7 +39,8 @@ const App = () => {
                 <h3>{data.title}</h3>
                 <h4 style={{ margin: "15px 0" }}>{data.subtitle}</h4>
             </div>
-            <Numbers numbers={numbers} />
+
+            <Colors />
         </div>
     );
 };
