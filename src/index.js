b763f5348201814467a1daf936e0f486e6ae15b3
Sema92m <sema92m@wp.pl>
2023-02-08 18:31:29 +0100

    Add ex3 day 6 complete

--- src/index.js
+++ src/index.js
@@ -2,45 +2,77 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-const hexaColor = () => {
-    const str = "0123456789abcdef";
-    let hexa = "#";
-    let index;
-    for (let i = 0; i < 6; i++) {
-        index = Math.floor(Math.random() * str.length);
-        hexa += str[index];
-    }
-    return hexa;
-};
+// const hexaColor = () => {
+//     const str = "0123456789abcdef";
+//     let hexa = "#";
+//     let index;
+//     for (let i = 0; i < 6; i++) {
+//         index = Math.floor(Math.random() * str.length);
+//         hexa += str[index];
+//     }
+//     return hexa;
+// };
 
+// const Colors = () => {
+//     let arr= [];
+//     for (let i = 0; i < 32; i++) {
+//         arr.push(hexaColor());
+//     }
+//     const color1 = arr.map((elem) => (
+//         <li key={elem} className="li-colors" style={{ backgroundColor: elem }}>
+//             {elem}
+//         </li>
+//     ));
+//     return <ul className="numbers-grid">{color1}</ul>;
+// };
 
-const Colors = () => {
-    let arr= [];
-    for (let i = 0; i < 32; i++) {
-        arr.push(hexaColor());
-    }
-    const color1 = arr.map((elem) => (
-        <li key={elem} className="li-colors" style={{ backgroundColor: elem }}>
-            {elem}
+const tenHighestPopulation = [
+    { country: "World", population: 7693165599 },
+    { country: "China", population: 1377422166 },
+    { country: "India", population: 1295210000 },
+    { country: "USA", population: 323947000 },
+    { country: "Indonesia", population: 258705000 },
+    { country: "Brazil", population: 206135893 },
+    { country: "Pakistan", population: 194125062 },
+    { country: "Nigeria", population: 186988000 },
+    { country: "Bangladesh", population: 161006790 },
+    { country: "Russian", population: 146599183 },
+    { country: "Japan", population: 126960000 },
+];
+function orangeLineWidth(elem) {
+    return (elem.population * 100) / 7693165599 + "%";
+}
+
+const Countries = () => {
+    const country = tenHighestPopulation.map((elem) => (
+        <li key={elem.country} className="li-countries">
+            <div className="country-name">{elem.country}</div>
+            <div
+                className="country-orangeLine"
+                style={{ width: orangeLineWidth(elem) }}
+            ></div>
+            <div className="country-population">
+                {elem.population.toLocaleString("en-US")}
+            </div>
         </li>
     ));
-    return <ul className="numbers-grid">{color1}</ul>;
+    return <ul className="countries-container">{country}</ul>;
 };
-
 const App = () => {
     const data = {
         title: "30 Days Of React",
-        subtitle: "Hexadencimal Colors",
+        subtitle: "World population",
+        titleSmall: "Ten most populated countries",
     };
 
     return (
         <div className="container6">
             <div style={{ textAlign: "center" }}>
-                <h3>{data.title}</h3>
-                <h4 style={{ margin: "15px 0" }}>{data.subtitle}</h4>
+                <h1 style={{ fontWeight: "900" }}>{data.title}</h1>
+                <h3 style={{ margin: "10px 0" }}>{data.subtitle}</h3>
+                <h6 style={{ marginBottom: "15px" }}>{data.titleSmall}</h6>
             </div>
-
-            <Colors />
+            <Countries />
         </div>
     );
 };
