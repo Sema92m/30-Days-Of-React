8b098b3b1d88f8ad7a2018c8c22680bccad4f683
Simon Bryczkowski <98832528+Sema92m@users.noreply.github.com>
2023-02-18 12:10:05 +0100

    Day 16 higher order component

--- src/App.js
+++ src/App.js
@@ -1,38 +1,101 @@
 import React from "react";
-// import Header from "./components/header/Header";
-import styled from "styled-components";
-import validator from 'validator';
-const Title = styled.h1`
-    font-size: 50px;
-    font-weight: 300;
-`;
-const Header = styled.header`
-    background-color: #61dbfb;
-    padding: 25;
-    padding: 10px;
-    margin: 0;
-`;
-const Input =()=> 
-  ( <div>
-    <label htmlFor=""></label>
-      <input type="email" />
-  </div>)
+const Button = ({ onClick, text, style }) => {
+    return (
+        <button onClick={onClick} style={style}>
+            {text}
+        </button>
+    );
+};
+
+const buttonWithStyles = (CompParam, name = "default") => {
+    const colors = [
+        {
+            name: "default",
+            backgroundColor: "#e7e7e7",
+            color: "#000000",
+        },
+        {
+            name: "react",
+            backgroundColor: "#61dbfb",
+            color: "#ffffff",
+        },
+        {
+            name: "success",
+            backgroundColor: "#4CAF50",
+            color: "#ffffff",
+        },
+        {
+            name: "info",
+            backgroundColor: "#2196F3",
+            color: "#ffffff",
+        },
+        {
+            name: "warning",
+            backgroundColor: "#ff9800",
+            color: "#ffffff",
+        },
+        {
+            name: "danger",
+            backgroundColor: "#f44336",
+            color: "#ffffff",
+        },
+    ];
+    const { backgroundColor, color } = colors.find((el) => el.name === name);
+
+    const buttonStyles = {
+        backgroundColor,
+        padding: "10px 45px",
+        border: "none",
+        borderRadius: 3,
+        margin: 3,
+        cursor: "pointer",
+        fontSize: "1.25rem",
+        color,
+    };
+    return (props) => {
+        return <CompParam {...props} style={buttonStyles} />;
+    };
+};
+
+const NewButton = buttonWithStyles(Button);
+const ReactButton = buttonWithStyles(Button, "react");
+const InfoButton = buttonWithStyles(Button, "info");
+const SuccessButton = buttonWithStyles(Button, "success");
+const WarningButton = buttonWithStyles(Button, "warning");
+const DangerButton = buttonWithStyles(Button, "danger");
 
 class App extends React.Component {
-  
     render() {
         return (
             <div className="App">
-                <Header>
-                    <div>
-                        <Title>30 Days Of React</Title>
-                        <h2>Getting Started React</h2>
-                        <h3>JavaScript Library</h3>
-                        <p>Instructor: Asabeneh Yetayeh</p>
-                        <small>Oct 15, 2020</small>
-                    <Input/>
-                    </div>
-                </Header>
+                <Button
+                    text="No Style"
+                    onClick={() => alert("I am not styled yet")}
+                />
+                <NewButton
+                    text="Styled Button"
+                    onClick={() => alert("I am the default style")}
+                />
+                <ReactButton
+                    text="React"
+                    onClick={() => console.log('123')}
+                />
+                <InfoButton
+                    text="Info"
+                    onClick={() => alert("I am styled with info color")}
+                />
+                <SuccessButton
+                    text="Success"
+                    onClick={() => alert("I am successful")}
+                />
+                <WarningButton
+                    text="Warning"
+                    onClick={() => alert("I warn you many times")}
+                />
+                <DangerButton
+                    text="Danger"
+                    onClick={() => alert("Oh no, you can not restore it")}
+                />
             </div>
         );
     }
