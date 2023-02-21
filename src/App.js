15071ed5e815355dd03db29016d677dac9341368
Simon Bryczkowski <98832528+Sema92m@users.noreply.github.com>
2023-02-21 21:33:35 +0100

    Day 18 started

--- src/App.js
+++ src/App.js
@@ -1,307 +1,82 @@
 import React, { Component } from "react";
-import {
-    BrowserRouter as Router,
-    Route,
-    Switch,
-    NavLink,
-    Redirect,
-    Prompt,
-} from "react-router-dom";
 
-// Home component
-const Home = (props) => <h1>Welcome Home</h1>;
-// About component
-const About = (props) => <h1>About Us</h1>;
-// Contact component
-const Contact = (props) => <h1>Contact us</h1>;
-// Challenge component
-
-const challenges = [
-    {
-        name: "30 Days Of Python",
-        description:
-            "30 Days of Python challenge is a step by step guide to learn Python in 30 days.",
-        status: "completed",
-        days: 30,
-        level: "Beginners to Advanced",
-        duration: "20 Nov 2019 - 20 Dec 2019",
-        slug: "pyhton",
-        url: "https://github.com/https://https://github.com/Asabeneh/30-Days-Of-Python.com/Asabeneh/30-Days-Of-JavaScript/30-Days-Of-React",
-        author: {
-            firstName: "Asabeneh",
-            lastName: "Yetayeh",
-        },
-    },
-    {
-        name: "30 Days Of JavaScript",
-        description:
-            "30 Days of JavaScript challenge is a step by step guide to learn JavaScript in 30 days.",
-        status: "completed",
-        days: 30,
-        level: "Beginners to Advanced",
-        duration: "1 Jan 2020 - 30 Jan 2020",
-        slug: "javascript",
-        url: "https://github.com/Asabeneh/30-Days-Of-JavaScript",
-        author: {
-            firstName: "Asabeneh",
-            lastName: "Yetayeh",
-        },
-    },
-    {
-        name: "30 Days Of React",
-        description:
-            "30 Days of React challenge is a step by step guide to learn React in 30 days.",
-        status: "ongoing",
-        days: 30,
-        level: "Beginners to Advanced",
-        duration: "1 Oct 2020- 30 Oct 2020",
-        slug: "react",
-        url: "https://github.com/Asabeneh/30-Days-Of-React",
-        author: {
-            firstName: "Asabeneh",
-            lastName: "Yetayeh",
-        },
-    },
-    {
-        name: "30 HTML and CSS",
-        description:
-            "30 Days of HTML and CSS challenge is a step by step guide to learn HTML and CSS in 30 days.",
-
-        status: "coming",
-        days: 30,
-        level: "Beginners to Advanced",
-        duration: "",
-        slug: "html-and-css",
-        url: "",
-        author: {
-            firstName: "Asabeneh",
-            lastName: "Yetayeh",
-        },
-    },
-    {
-        name: "30 ReactNative",
-        description:
-            "30 Days of ReactNative challenge is a step by step guide to learn ReactNative in 30 days.",
-        status: "coming",
-        days: 30,
-        level: "Beginners to Advanced",
-        duration: "",
-        slug: "reactnative",
-        url: "",
-        author: {
-            firstName: "Asabeneh",
-            lastName: "Yetayeh",
-        },
-    },
-    {
-        name: "30 Data Analysis",
-        description:
-            "30 Days of Data Analysis challenge  is a step by step guide to learn about data, data visualization and data analysis in 30 days.",
-        status: "coming",
-        days: 30,
-        level: "Beginners to Advanced",
-        duration: "",
-        slug: "data-analysis",
-        url: "",
-        author: {
-            firstName: "Asabeneh",
-            lastName: "Yetayeh",
-        },
-    },
-    {
-        name: "30 Machine Learning",
-        description:
-            "30 Days of Machine learning challenge  is a step by step guide to learn data cleaning, machine learning models and predictions in 30 days.",
-        status: "coming",
-        days: 30,
-        level: "Beginners to Advanced",
-        duration: "",
-        slug: "machine-learning",
-        url: "",
-        author: {
-            firstName: "Asabeneh",
-            lastName: "Yetayeh",
-        },
-    },
-];
-
-const Challenge = ({
-    challenge: {
-        name,
-        description,
-        status,
-        days,
-        level,
-        duration,
-        author: { firstName, lastName },
-    },
-}) => (
-    <div>
-        <h1>{name}</h1>
-        <p>{level}</p>
+const Country = ({
+  country: { name, capital, flag, languages, population, currency },
+}) => {
+  const formatedCapital =
+    capital.length > 0 ? (
+      <>
+        <span>Capital: </span>
+        {capital}
+      </>
+    ) : (
+      ''
+    )
+  const formatLanguage = languages.length > 1 ? `Languages` : `Language`
+  console.log(languages)
+  return (
+    <div className='country'>
+      <div className='country_flag'>
+        <img src={flag} alt={name} />
+      </div>
+      <h3 className='country_name'>{name.toUpperCase()}</h3>
+      <div class='country_text'>
+        <p>{formatedCapital}</p>
         <p>
-            Author: {firstName} {lastName}
+          <span>{formatLanguage}: </span>
+          {languages.map((language) => language.name).join(', ')}
         </p>
-        {duration && (
-            <>
-                {" "}
-                <small>{duration}</small> <br />
-            </>
-        )}
-        <small>Number of days: {days}</small>
-
-        <p>{description}</p>
+        <p>
+          <span>Population: </span>
+          {population.toLocaleString()}
+        </p>
+        <p>
+          <span>Currency: </span>
+          {currency}
+        </p>
+      </div>
     </div>
-);
+  )
+}
 
-const Challenges = (props) => {
-    const path = props.location.pathname;
-    const slug = path.split("/").slice(path.split("/").length - 1)[0];
-    const challenge = challenges.find((challenge) => challenge.slug === slug);
-
-    return (
-        <div>
-            <h1>30 Days Of React Challenge</h1>
-            <ul>
-                {challenges.map(({ name, slug }) => (
-                    <li>
-                        <NavLink to={`/challenges/${slug}`}>{name}</NavLink>
-                    </li>
-                ))}
-            </ul>
-            <Switch>
-                <Route
-                    exact
-                    path={"/challenges"}
-                    component={() => <h1>Choose any of the challenges</h1>}
-                />
-                <Route
-                    path={path}
-                    component={(props) => <Challenge challenge={challenge} />}
-                />
-            </Switch>
-        </div>
-    );
-};
-
-const NotFound = (props) => <h1>The page your looking for not found</h1>;
-const Navbar = ({ username }) => (
-    <ul>
-        <li>
-            <NavLink to="/">Home</NavLink>
-        </li>
-        <li>
-            <NavLink to="/about">About</NavLink>
-        </li>
-        <li>
-            <NavLink to="/contact">Contact</NavLink>
-        </li>
-        <li>
-            <NavLink to={`/user/${username}`}>User</NavLink>
-        </li>
-        <li>
-            <NavLink to="/challenges">Challenges</NavLink>
-        </li>
-    </ul>
-);
-
-const User = ({ match, isLoggedIn, handleLogin }) => {
-    const username = match.params.username;
-    return (
-        <div>
-            {isLoggedIn ? (
-                <>
-                    <h1>Welcome {username} to the challenge</h1>
-                    <small>
-                        Now, you can navigate through all the challenges
-                    </small>{" "}
-                    <br />
-                </>
-            ) : (
-                <p>Please login in to access the challenges </p>
-            )}
-            <button onClick={handleLogin}>
-                {isLoggedIn ? "Logout" : "Login"}
-            </button>
-        </div>
-    );
-};
-
-const Welcome = ({ handleLogin, isLoggedIn }) => {
-    return (
-        <div>
-            {isLoggedIn ? "Welcome to the challenge" : <p>Please login in </p>}
-            <button onClick={handleLogin}>
-                {isLoggedIn ? "Logout" : "Login"}
-            </button>
-        </div>
-    );
-};
 class App extends Component {
-    state = {
-        isLoggedIn: false,
-        firstName: "Asabeneh",
-    };
-    handleLogin = () => {
+  state = {
+    data: [],
+  }
+
+  componentDidMount() {
+    const url = 'https://restcountries.eu/rest/v2/all'
+    fetch(url)
+      .then((response) => {
+        return response.json()
+      })
+      .then((data) => {
+        console.log(data)
         this.setState({
-            isLoggedIn: !this.state.isLoggedIn,
-        });
-    };
-    render() {
-        return (
-            <Router>
-                <div className="App">
-                    <Navbar username={this.state.firstName} />
+          data,
+        })
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
 
-                    <Prompt
-                        message={({ pathname }) => {
-                            return this.state.isLoggedIn &&
-                                pathname.includes("/user/Asabeneh")
-                                ? "Are you sure you want to logout?"
-                                : true;
-                        }}
-                    />
-
-                    <Switch>
-                        <Route path="/about" component={About} />
-                        <Route path="/contact" component={Contact} />
-                        <Route
-                            path="/user/:username"
-                            component={(props) => (
-                                <User
-                                    {...props}
-                                    isLoggedIn={this.state.isLoggedIn}
-                                    handleLogin={this.handleLogin}
-                                />
-                            )}
-                        />
-                        <Route
-                            path="/login"
-                            component={(props) => (
-                                <Welcome
-                                    {...props}
-                                    isLoggedIn={this.state.isLoggedIn}
-                                    handleLogin={this.handleLogin}
-                                />
-                            )}
-                        />
-                        <Route
-                            path="/challenges"
-                            component={(props) => {
-                                return this.state.isLoggedIn ? (
-                                    <Challenges {...props} />
-                                ) : (
-                                    <Redirect to="/user/asabeneh" />
-                                );
-                            }}
-                        />
-                        <Route exact path="/" component={Home} />
-                        <Route component={NotFound} />
-                    </Switch>
-                </div>
-            </Router>
-        );
-    }
+  render() {
+    return (
+      <div className='App'>
+        <h1>React Component Life Cycle</h1>
+        <h1>Calling API</h1>
+        <div>
+          <p>There are {this.state.data.length} countries in the api</p>
+          <div className='countries-wrapper'>
+            {this.state.data.map((country) => (
+              <Country country={country} />
+            ))}
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
