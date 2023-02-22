9262a50478592639ecf9174769c5b1a8b82d4af2
Simon Bryczkowski <98832528+Sema92m@users.noreply.github.com>
2023-02-22 21:40:33 +0100

    Start ReactHOOKS

--- src/App.js
+++ src/App.js
@@ -1,82 +1,219 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
+import myImg from './images/img1.png';
 
-const Country = ({
-  country: { name, capital, flag, languages, population, currency },
-}) => {
-  const formatedCapital =
-    capital.length > 0 ? (
-      <>
-        <span>Capital: </span>
-        {capital}
-      </>
-    ) : (
-      ''
-    )
-  const formatLanguage = languages.length > 1 ? `Languages` : `Language`
-  console.log(languages)
+// const showDate = (time) => {
+//   const months = [
+//     'January',
+//     'February',
+//     'March',
+//     'April',
+//     'May',
+//     'June',
+//     'July',
+//     'August',
+//     'September',
+//     'October',
+//     'November',
+//     'December',
+//   ]
+
+//   const month = months[time.getMonth()].slice(0, 3)
+//   const year = time.getFullYear()
+//   const date = time.getDate()
+//   return ` ${month} ${date}, ${year}`
+// }
+
+// User Card Component
+const UserCard = ({ user: { firstName, lastName, image } }) => (
+  <div className='user-card'>
+    <img src={image} alt={firstName} />
+    <h2>
+      {firstName}
+      {lastName}
+    </h2>
+  </div>
+)
+
+// A button component
+const Button = ({ text, onClick, style }) => (
+  <button style={style} onClick={onClick}>
+    {text}
+  </button>
+)
+
+// CSS styles in JavaScript Object
+const buttonStyles = {
+  backgroundColor: '#61dbfb',
+  padding: 10,
+  border: 'none',
+  borderRadius: 5,
+  margin: 3,
+  cursor: 'pointer',
+  fontSize: 18,
+  color: 'white',
+}
+
+const Header = (props) => {
+  const {
+    welcome,
+    title,
+    subtitle,
+    author: { firstName, lastName },
+    date,
+  } = props.data
+
   return (
-    <div className='country'>
-      <div className='country_flag'>
-        <img src={flag} alt={name} />
+    <header style={props.styles}>
+      <div className='header-wrapper'>
+        <h1>{welcome}</h1>
+        <h2>{title}</h2>
+        <h3>{subtitle}</h3>
+        <p>
+          {firstName} {lastName}
+        </p>
+        <small>{date}</small>
       </div>
-      <h3 className='country_name'>{name.toUpperCase()}</h3>
-      <div class='country_text'>
-        <p>{formatedCapital}</p>
-        <p>
-          <span>{formatLanguage}: </span>
-          {languages.map((language) => language.name).join(', ')}
-        </p>
-        <p>
-          <span>Population: </span>
-          {population.toLocaleString()}
-        </p>
-        <p>
-          <span>Currency: </span>
-          {currency}
-        </p>
-      </div>
-    </div>
+    </header>
   )
 }
 
-class App extends Component {
-  state = {
-    data: [],
-  }
+const Count = ({ count, addOne, minusOne }) => (
+  <div>
+    <h1>{count} </h1>
+    <div>
+      <Button text='+1' onClick={addOne} style={buttonStyles} />
+      <Button text='-1' onClick={minusOne} style={buttonStyles} />
+    </div>
+  </div>
+)
 
-  componentDidMount() {
-    const url = 'https://restcountries.eu/rest/v2/all'
-    fetch(url)
-      .then((response) => {
-        return response.json()
-      })
-      .then((data) => {
-        console.log(data)
-        this.setState({
-          data,
-        })
-      })
-      .catch((error) => {
-        console.log(error)
-      })
-  }
+// TechList Component
+const TechList = (props) => {
+  const { techs } = props
+  const techsFormatted = techs.map((tech) => <li key={tech}>{tech}</li>)
+  return techsFormatted
+}
 
-  render() {
-    return (
-      <div className='App'>
-        <h1>React Component Life Cycle</h1>
-        <h1>Calling API</h1>
-        <div>
-          <p>There are {this.state.data.length} countries in the api</p>
-          <div className='countries-wrapper'>
-            {this.state.data.map((country) => (
-              <Country country={country} />
-            ))}
-          </div>
-        </div>
+// Main Component
+const Main = (props) => {
+  const {
+    techs,
+    user,
+    greetPeople,
+    handleTime,
+    changeBackground,
+    count,
+    addOne,
+    minusOne,
+  } = props
+  return (
+    <main>
+      <div className='main-wrapper'>
+        <p>Prerequisite to get started react.js:</p>
+        <ul>
+          <TechList techs={techs} />
+        </ul>
+        <UserCard user={user} />
+        <Button
+          text='Greet People'
+          onClick={greetPeople}
+          style={buttonStyles}
+        />
+        <Button text='Show Time' onClick={handleTime} style={buttonStyles} />
+        <Button
+          text='Change Background'
+          onClick={changeBackground}
+          style={buttonStyles}
+        />
+        <Count count={count} addOne={addOne} minusOne={minusOne} />
       </div>
-    )
+    </main>
+  )
+}
+
+// Footer Component
+const Footer = (props) => {
+  return (
+    <footer>
+      <div className='footer-wrapper'>
+        <p>Copyright {props.date.getFullYear()}</p>
+      </div>
+    </footer>
+  )
+}
+
+const App = (props) => {
+  const [count, setCount] = useState(0)
+  const [backgroundColor, setBackgroundColor] = useState('')
+
+  const showDate = (time) => {
+    const months = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December',
+    ]
+
+    const month = months[time.getMonth()].slice(0, 3)
+    const year = time.getFullYear()
+    const date = time.getDate()
+    return ` ${month} ${date}, ${year}`
   }
+  const addOne = () => {
+    setCount(count + 1)
+  }
+
+  // method which subtract one to the state
+  const minusOne = () => {
+    setCount(count - 1)
+  }
+  const handleTime = () => {
+    alert(showDate(new Date()))
+  }
+  const greetPeople = () => {
+    alert('Welcome to 30 Days Of React Challenge, 2020')
+  }
+  const changeBackground = () => {}
+
+  const data = {
+    welcome: 'Welcome to 30 Days Of React',
+    title: 'Getting Started React',
+    subtitle: 'JavaScript Library',
+    author: {
+      firstName: 'Asabeneh',
+      lastName: 'Yetayeh',
+    },
+    date: 'Oct 7, 2020',
+  }
+  const techs = ['HTML', 'CSS', 'JavaScript']
+
+  const user = { ...data.author, image: myImg }
+
+  return (
+    <div className='app'>
+      {backgroundColor}
+      <Header data={data} />
+      <Main
+        user={user}
+        techs={techs}
+        handleTime={handleTime}
+        greetPeople={greetPeople}
+        changeBackground={changeBackground}
+        addOne={addOne}
+        minusOne={minusOne}
+        count={count}
+      />
+      <Footer date={new Date()} />
+    </div>
+  )
 }
 
 export default App;
